090df8c7c3696a6dd5b21f39f3a34b541dcf3662
MINSIX <alstlr0718@naver.com>
2022-11-29 20:06:45 +0900

    merge

--- 테트리스만들기/Board.cs
+++ 테트리스만들기/Board.cs
@@ -103,28 +103,28 @@ namespace 테트리스만들기
                 }
             }
         }
-        internal void DAB()
-        {
-            count = 0;
-            int x = -1;
-            for (int yy = GameRule.BY -1; yy >= 0; yy--)
-            {
-                for (int xx = GameRule.BX - 1; xx >= 0; xx--)
-                {
-                    if (board[xx, yy] != 0) { x = xx;break; }
-                }if(x!=-1)
-                count++;
-            }
-            for (int y=GameRule.BX-1; y > 0; y--)
-            {
-                for (int xx = 0; xx < GameRule.BX; xx++)
-                {
-                    //윗줄의 내용을 아랫줄에 줄복사하는코드
-                    if(board[xx, y] != 0) { board[xx, y] = board[xx, y + count]; }
+        //internal void DAB()
+        //{
+        //    count = 0;
+        //    int x = -1;
+        //    for (int yy = GameRule.BY -1; yy >= 0; yy--)
+        //    {
+        //        for (int xx = GameRule.BX - 1; xx >= 0; xx--)
+        //        {
+        //            if (board[xx, yy] != 0) { x = xx;break; }
+        //        }if(x!=-1)
+        //        count++;
+        //    }
+        //    for (int y=GameRule.BX-1; y > 0; y--)
+        //    {
+        //        for (int xx = 0; xx < GameRule.BX; xx++)
+        //        {
+        //            //윗줄의 내용을 아랫줄에 줄복사하는코드
+        //            if(board[xx, y] != 0) { board[xx, y] = board[xx, y + count]; }
                    
-                }
-            }
-        }
+        //        }
+        //    }
+        //}
         private void CheckLines(int y)
         {
             int yy = 0;
