c1f3d86eb9eed2d4a26a99d9cc27c49614e0dae3
MINSIX <alstlr0718@naver.com>
2022-11-29 10:26:21 +0900

    bugclear -next block on

--- 테트리스만들기/Board.cs
+++ 테트리스만들기/Board.cs
@@ -6,61 +6,58 @@ using System.Threading.Tasks;
 
 namespace 테트리스만들기
 {
-    internal class Board
+     class Board
     {
-        internal static Board GameBoard
-        {
-            get;
-            private set;
-        }
-        static Board()
-        {
-            GameBoard = new Board();
-        }
-        Board()
-        { }
-        int[,] board = new int[GameRule.BX, GameRule.BY];
-        internal int this[int x, int y]
-        {
-            get
+            internal static Board GameBoard
             {
-                return board[x, y];
+                get;
+                private set;
             }
-        }    
-        //못움직임 !!
-        internal bool MoveEnable(int bn,int tn, int x, int y)
-        {
-            for(int xx = 0; xx < 4; xx++)
+            static Board()
             {
-                for(int yy = 0; yy < 4; yy++)
+                GameBoard = new Board();
+            }
+            Board()
+            {
+            }
+            int[,] board = new int[GameRule.BX, GameRule.BY];
+            internal int this[int x, int y]
+            {
+                get
                 {
-                    if (BlockValue.bvals[bn, tn, xx, yy] != 0)
+                    return board[x, y];
+                }
+            }
+            internal bool MoveEnable(int bn, int tn, int x, int y)
+            {
+                for (int xx = 0; xx < 4; xx++)
+                {
+                    for (int yy = 0; yy < 4; yy++)
                     {
-                        if(board[x+xx,y+yy]!=0) //x는 현재좌표 xx는 상대좌표
+                        if (BlockValue.bvals[bn, tn, xx, yy] != 0)
                         {
-                            return false;
+                            if (board[x + xx, y + yy] != 0)
+                            {
+                                return false;
+                            }
                         }
                     }
                 }
+                return true;
             }
-            
-            return true;
-        }
-        
-        internal void Store(int bn, int tn, int x, int y)//쌓기
-        {
-            for(int xx = 0; xx < 4; x++)
+            internal void Store(int bn, int turn, int x, int y)
             {
-                for(int yy = 0; yy < 4; yy++)
+                for (int xx = 0; xx < 4; xx++)
                 {
-                    if (((x + xx) >= 0) && (x + xx < GameRule.BX) && (y + yy >= 0) && (y + yy < GameRule.BY))
+                    for (int yy = 0; yy < 4; yy++)
                     {
-                        //블록에 있는 값과 보드에 있는 값 합치기(쌓는효과)
-                        board[x + xx, y + yy] += BlockValue.bvals[bn, tn, xx, yy];
-                        
+                        if (((x + xx) >= 0) && (x + xx < GameRule.BX) && (y + yy >= 0) && (y + yy < GameRule.BY))
+                        {
+
+                            board[x + xx, y + yy] += BlockValue.bvals[bn, turn, xx, yy];
+                        }
                     }
                 }
             }
         }
     }
-}
--- 테트리스만들기/Game.cs
+++ 테트리스만들기/Game.cs
@@ -7,16 +7,37 @@ using System.Threading.Tasks;
 
 namespace 테트리스만들기
 {
-    internal class Game
+     class Game
     {
         Diagram now;
         Board gboard=Board.GameBoard; //단일객체
-    
-    
-      
-     
 
-        //단일객체
+
+
+
+
+        internal Point NowPosition
+        {
+            get
+            {
+                return new Point(now.X, now.Y);
+            }
+        }
+        internal int BlockNum
+        {
+            get
+            {
+                return now.BlockNum;
+            }
+        }
+        internal int Turn
+        {
+            get
+            {
+                return now.Turn;
+            }
+        }
+
         internal static Game Singleton
         {
             get;
@@ -37,39 +58,22 @@ namespace 테트리스만들기
         {
             now = new Diagram();
         }
-        internal Point NowPosition
-        {
-            get
-            {
-                return new Point(now.X, now.Y); //now의 x,y 반환
-            }
-        }
-        internal int BlockNum
-        {
-            get { return now.BlockNum; }
-        }
-        internal int Turn
-        {
-            get { return now.Turn; }
-        }
-        #region
-    
-        #endregion
         internal bool MoveLeft()
         {
             for (int xx = 0; xx < 4; xx++)
             {
                 for (int yy = 0; yy < 4; yy++)
                 {
-                    if (BlockValue.bvals[now.BlockNum, now.Turn, xx, yy] != 0)
+                    if (BlockValue.bvals[now.BlockNum, Turn, xx, yy] != 0)
                     {
                         if (now.X + xx <= 0)
                         {
                             return false;
-                        }//위치 멈추게하기(벽안넘도록)
+                        }
                     }
                 }
             }
+
             if (gboard.MoveEnable(now.BlockNum, Turn, now.X - 1, now.Y))
             {
                 now.MoveLeft();
@@ -77,18 +81,19 @@ namespace 테트리스만들기
             }
             return false;
         }
+
         internal bool MoveRight()
         {
             for (int xx = 0; xx < 4; xx++)
             {
                 for (int yy = 0; yy < 4; yy++)
                 {
-                    if (BlockValue.bvals[now.BlockNum, now.Turn, xx, yy] != 0)
+                    if (BlockValue.bvals[now.BlockNum, Turn, xx, yy] != 0)
                     {
-                        if (now.X + xx+1 >= GameRule.BX)
+                        if ((now.X + xx + 1) >= GameRule.BX)
                         {
                             return false;
-                        }//위치 멈추게하기(벽안넘도록)
+                        }
                     }
                 }
             }
@@ -99,6 +104,7 @@ namespace 테트리스만들기
             }
             return false;
         }
+
         internal bool MoveDown()
         {
             for (int xx = 0; xx < 4; xx++)
@@ -107,47 +113,48 @@ namespace 테트리스만들기
                 {
                     if (BlockValue.bvals[now.BlockNum, Turn, xx, yy] != 0)
                     {
-                        if ((now.Y + yy + 1)>= GameRule.BY)
+                        if ((now.Y + yy + 1) >= GameRule.BY)
                         {
-                           // gboard.Store(now.BlockNum, Turn, now.X, now.Y);
-                          
+                            gboard.Store(now.BlockNum, Turn, now.X, now.Y);
+                            now.Reset();
                             return false;
-                        }//위치 멈추게하기(벽안넘도록)
+                        }
                     }
                 }
             }
-            if (gboard.MoveEnable(now.BlockNum, Turn, now.X, now.Y+1)) 
+            if (gboard.MoveEnable(now.BlockNum, Turn, now.X, now.Y + 1))
             {
                 now.MoveDown();
                 return true;
             }
             gboard.Store(now.BlockNum, Turn, now.X, now.Y);
-
+            now.Reset();
             return false;
         }
+
         internal bool MoveTurn()
         {
             for (int xx = 0; xx < 4; xx++)
             {
                 for (int yy = 0; yy < 4; yy++)
                 {
-                    if (BlockValue.bvals[now.BlockNum, (now.Turn+1)%4, xx, yy] != 0)
+                    if (BlockValue.bvals[now.BlockNum, (Turn + 1) % 4, xx, yy] != 0)
                     {
-                        if (((now.X+xx)<0)||(now.X+xx)>=GameRule.BX||((now.Y+yy)>=GameRule.BY))
+                        if (((now.X + xx) < 0) || ((now.X + xx) >= GameRule.BX) || ((now.Y + yy) >= GameRule.BY))
                         {
-
                             return false;
-                        }//위치 멈추게하기(벽안넘도록)
+                        }
                     }
                 }
             }
             if (gboard.MoveEnable(now.BlockNum, (Turn + 1) % 4, now.X, now.Y))
-            { 
+            {
                 now.MoveTurn();
                 return true;
             }
             return false;
-            }
+        }
+
         internal void Next()
         {
             now.Reset();
